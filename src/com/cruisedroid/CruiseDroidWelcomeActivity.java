0f208c73d48cabe0afd71fbd9bc5bdf3bb80e190
Amiruddin Nagri <amir.nagri@gmail.com>
2010-02-19 13:52:26 +0530

    Amir : Checked in project setup code

---
+++ src/com/cruisedroid/CruiseDroidWelcomeActivity.java
@@ -0,0 +1,13 @@
+package com.cruisedroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CruiseDroidWelcomeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
